fd954e370d9ace6d4a824284b5dabf88231f5011
IamHDA <huaduyanh2010@gmail.com>
2025-03-06 00:04:21 +0700

    remain files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from "./component/Header.jsx";
+import HomePage from "./component/HomePage.jsx";
+import Footer from "./component/Footer.jsx";
+
+
+const MyComponent = () => {
+    return (
+        <div>
+            <Header />
+            <HomePage />
+            <Footer />
+        </div>
+    );
+};
+
+export default MyComponent;
